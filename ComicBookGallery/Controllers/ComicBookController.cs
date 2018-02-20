7db7284fca12ceef7b5bc38a1216933cc1c69f89
SASYWEB <contact@saysweb.com>
2018-02-20 09:26:43 +0530

    Added ComicBookController

---
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        // GET: ComicBook
+        public ContentResult Detail()
+        {
+            return new ContentResult() {
+                Content = "Hello from Comic Book's Controller"
+            };
+        }
+    }
+}
